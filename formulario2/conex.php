c8bba3df7ad8b6187228817787fa94547112a7e7
Duban <mr.garcialml@gmail.com>
2019-07-18 11:40:05 -0500

    ya esta

--- formulario2/conex.php
+++ formulario2/conex.php
@@ -2,7 +2,7 @@
 	function conectar(){
 		$servidor = "104.154.199.25";
 		$usuario	= "root";
-		$clave		= "";
+		$clave		= "a123";
 		$bd				= "formulariosdb";
 		
 		if(!($link = mysql_connect($servidor,$usuario,$clave))){
